94dd84bbe8b359479a60373b863ea5e51dedcbdf
MaliSoi <malalasoifer@gmail.com>
2025-10-18 11:34:12 -0300

    Guardando ejercicio 1 clase-05.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 import React from "react";
-import LoginStatus from "./components/LoginStatus";
-import Notifications from "./components/Notifications";
-import LoadingStatus from "./components/LoadingStatus";
-import Messages from "./components/Messages";
+import Counter from "./components/Counter";
+
 
 
 function App () {
-  const user = {
-    name:"Malala",
-    isLoggedIn: true, //pruebo despues cambiar a false
-};
-
-//simulación de estado de carga de datos
-const data = true; //cambio a false para ver "Loading..."
-
-const hasMessages = true; //probar con false
-
-   return (
+  return (
     <div>
-   <h2>Ejercicio 1 - if/else</h2>
-   <LoginStatus name={user.name} isLoggedIn={user.isLoggedIn}/>
-   
-   
-   <h2>Ejercicio 2 - null</h2>
-   <Notifications count={0}/> 
-
-   <h2>Ejercicio 3 - Carga de datos</h2>
-   <LoadingStatus isLoading={!data}/>
-
-   <h2>Ejercicio 4 - hay mensajes con &&</h2>
-   <Messages hasMessages={hasMessages}/>
-   
+    <h1>Ejercicios Clase 5.1</h1> 
+    <h2>Ejercicio 1 : Contador +  - </h2>
+    <Counter/>
    
    </div>
   );
--- src/components/LoadingStatus.jsx
+++
@@ -1,6 +0,0 @@
-const LoadingStatus = ({ isLoading }) => {
-    return <p>{isLoading ? "Cargando..." : "Datos Cargados"}</p>;
-    };
-
-    export default LoadingStatus;
-    
